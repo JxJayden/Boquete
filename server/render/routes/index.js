94c663d5466532532bfcda520deb30afdba59d9b
jayden <jxdjayden@gmail.com>
2017-05-11 17:18:53 +0800

    change render data

--- server/render/routes/index.js
+++ server/render/routes/index.js
@@ -4,10 +4,7 @@ const router = require('koa-router')(),
     logger = require('../../lib/log')('router')
 
 router
-    .get(['/:websiteId', '/:websiteId/*'], async(ctx, next) => {
-        ctx.websiteInfo = await _.getWebsiteInfo(ctx.params.websiteId)
-        await next()
-    })
+    .get(['/:websiteId', '/:websiteId/*'], getInfoAndSetCommonData)
     .get(['/:websiteId', '/:websiteId/index'], page.home)
     .get('/:websiteId/post', page.post_list)
     .get('/:websiteId/post/:postId', page.post_main)
@@ -15,3 +12,37 @@ router
     .get('/:websiteId/chat', page.chat)
 
 module.exports = router
+
+async function getInfoAndSetCommonData(ctx, next) {
+    ctx.websiteInfo = await _.getWebsiteInfo(ctx.params.websiteId)
+    let webSiteInfo = ctx.websiteInfo
+
+    if (webSiteInfo === null) {
+        throw {
+            message: '无该网站',
+            code: 404
+        }
+    }
+    let common = {
+        title: webSiteInfo.title,
+        logo:  `//api.cms.com/${webSiteInfo.logo}`,
+        nav: {
+            homeUrl: webSiteInfo.url,
+            nav: webSiteInfo.nav,
+            logo: `//api.cms.com/${webSiteInfo.logo}`
+        },
+        page: {
+            title: webSiteInfo.title,
+            description: webSiteInfo.description,
+            chaturl: webSiteInfo.url + '/chat'
+        },
+        footer: {
+            copyright: webSiteInfo.copyright,
+            description: webSiteInfo.description
+        }
+    }
+    ctx.state = {
+        common: common
+    }
+    await next()
+}
